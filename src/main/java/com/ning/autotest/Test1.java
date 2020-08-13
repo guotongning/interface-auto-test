24672ad95f0ae6e080cff89cdab55c554aacb30b
fwl <327813304@qqcom>
2020-08-13 23:46:56 +0800

    提交test1

--- src/main/java/com/ning/autotest/Test1.java
+++ src/main/java/com/ning/autotest/Test1.java
@@ -1,10 +1,27 @@
 package com.ning.autotest;
 
+import org.junit.After;
+import org.junit.Test;
+
+import javax.annotation.PreDestroy;
+
 /**
  * @Author: nicholas
  * @Date: 2020/8/13 21:50
  * @Descreption: 第一课：Junit的使用。
  */
 public class Test1 {
+    @
+    public void pre(){
+
+    }
+    @After
+    public void aft(){
+
+    }
+    @Test
+    public void test(){
+
+    }
 
 }
