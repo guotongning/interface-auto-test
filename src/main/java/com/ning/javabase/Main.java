d3d8de2c574af8db06abe4402764f581b95da1e1
guotongning <guotongning@58.com>
2020-08-14 00:08:24 +0800

    作业

--- src/main/java/com/ning/javabase/Main.java
+++ src/main/java/com/ning/javabase/Main.java
@@ -4,6 +4,11 @@ package com.ning.javabase;
  * @Author: nicholas
  * @Date: 2020/8/14 00:04
  * @Descreption: 课外：学习java语法
+ * 1. 创建一个类，有各种属性。（各种属性的意思是：各种类型的属性都要有）
+ * 2. 在这个类里面创建各种方法。（各种类型的方法都要有）
+ * 3. 填充方法体，自由填充。
+ * 4. 在一个方法里调用另一个方法。
+ * 5. 待定
  */
 public class Main {
 
