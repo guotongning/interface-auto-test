35d9547974cd3f8bef529e688df887a0cdd387d2
fwl <327813304@qqcom>
2020-08-13 23:53:38 +0800

    提交test1

--- src/main/java/com/ning/autotest/Test1.java
+++ src/main/java/com/ning/autotest/Test1.java
@@ -1,9 +1,8 @@
 package com.ning.autotest;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
-import javax.annotation.PreDestroy;
 
 /**
  * @Author: nicholas
@@ -11,17 +10,21 @@ import javax.annotation.PreDestroy;
  * @Descreption: 第一课：Junit的使用。
  */
 public class Test1 {
-    @
+    @Before
     public void pre(){
-
+        System.out.println("之前");
     }
     @After
     public void aft(){
-
+        System.out.println("之后");
     }
     @Test
     public void test(){
-
+        System.out.println("当中");
+    }
+    @Test
+    public void test1(){
+        System.out.println("当中1");
     }
 
 }
